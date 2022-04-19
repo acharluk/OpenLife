35428602c89fe5e78973093ba8abf246ccaea32e
acharluk <acharluk@gmail.com>
2022-04-19 20:55:50 +0000

    Build: (9f9f033) Use named routes, added random events

--- assets/index.6517984b.js
+++
@@ -1,1 +0,0 @@
-var e={id:"web-developer",title:"Web Developer",salary:2e4};const o={"web-developer":e};export{o as j};
---
+++ assets/index.d5dab4fa.js
@@ -0,0 +1,1 @@
+var e={id:"president",title:"President",salary:1e6,firedChance:.8},r={id:"web-developer",title:"Web Developer",salary:2e4,firedChance:.1};const d={"web-developer":r,president:e};export{d as j};
